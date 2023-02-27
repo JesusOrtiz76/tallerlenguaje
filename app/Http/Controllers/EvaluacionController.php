2ea02126a9d02d7f7be5d5cbc721433d25710770
Ivan B <44617333+IvanBR1@users.noreply.github.com>
2023-02-27 17:57:47 -0600

    Checando el metodo evaluacion.show, cambio de zona horaria

--- app/Http/Controllers/EvaluacionController.php
+++ app/Http/Controllers/EvaluacionController.php
@@ -5,13 +5,52 @@ namespace App\Http\Controllers;
 use App\Models\Evaluacion;
 use App\Models\Modulo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class EvaluacionController extends Controller
 {
-    public function show(Evaluacion $evaluacion)
+    public function show(Request $request, $id)
     {
-        return view('evaluaciones.show', compact('evaluacion'));
+        // Obtener la evaluación del usuario logueado
+        $evaluacion = Evaluacion::where('id', $id)
+            ->whereHas('users', function ($query) use ($request) {
+                $query->where('users.id', $request->user()->id);
+            })
+            //->with('topics.questions')
+            ->first();
+
+        // Verificar si la evaluación existe
+        if (!$evaluacion) {
+            return redirect()->back()->with('error', 'La evaluación no existe.');
+        }
+
+        // Verificar si la evaluación está completa
+        $user_evaluacion = $evaluacion->users->where('id', $request->user()->id)->first();
+
+        if (!$user_evaluacion) {
+            return redirect()->back()->with('warning', 'No ha completado la evaluación.');
+        }
+
+        if ($user_evaluacion->pivot->intentos >= 4) {
+            return redirect()->back()->with('warning', 'Ha alcanzado el máximo de intentos para esta evaluación.');
+        }
+
+        /*
+        if (Carbon::now()->diffInMinutes($user_evaluacion->updated_at) > 15) {
+
+            return Carbon::now()->diffInMinutes($user_evaluacion->updated_at);
+            //return redirect()->back()->with('warning', 'Ha excedido el tiempo máximo para completar esta evaluación.');
+        }
+        */
+
+        // Calcular el resultado de la evaluación
+        $result = $evaluacion->getResult($request->user()->id);
+
+        return $result;
+
+        // Mostrar la vista con el resultado de la evaluación
+        return view('evaluacions.show', compact('evaluacion', 'result'));
     }
 
     public function store(Request $request)
--- resources/views/evaluaciones/show.blade.php
+++ resources/views/evaluaciones/show.blade.php
@@ -1,1 +1,43 @@
-{{ $evaluacion }}
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Evaluación</div>
+
+                    <div class="card-body">
+                        @if ($evaluacion->attempts_left <= 0)
+                            <div class="alert alert-danger" role="alert">
+                                {{ __('No tiene más intentos para esta evaluación.') }}
+                            </div>
+                        @else
+                            <p>{{ __('Esta evaluación tiene un límite de tiempo de 15 minutos. Usted tiene :time_left minutos restantes.', ['time_left' => $evaluacion->time_left]) }}</p>
+                            <p>{{ __('Usted tiene :attempts_left intentos restantes para esta evaluación.', ['attempts_left' => $evaluacion->attempts_left]) }}</p>
+                            <form method="POST" action="{{ route('evaluations.submit', ['evaluation' => $evaluacion->id]) }}">
+                                @csrf
+
+                                @foreach ($evaluacion->questions as $question)
+                                    <div class="form-group">
+                                        <p>{{ $question->text }}</p>
+                                        @foreach ($question->options as $option)
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="radio" name="answers[{{ $question->id }}]" id="answer_{{ $option->id }}" value="{{ $option->id }}">
+                                                <label class="form-check-label" for="answer_{{ $option->id }}">
+                                                    {{ $option->text }}
+                                                </label>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                @endforeach
+
+                                <button type="submit" class="btn btn-primary">{{ __('Enviar') }}</button>
+                            </form>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
